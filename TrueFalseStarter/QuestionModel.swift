c69785baa0573e890743449c8457f6af6a2839a0
Nuno Trindade <nunomtrindade@gmail.com>
2016-08-22 23:28:17 +0100

    [True False App] Adds questions to the fixed questions array and fixes the random questions alghoritm

--- TrueFalseStarter/QuestionModel.swift
+++ TrueFalseStarter/QuestionModel.swift
@@ -14,27 +14,99 @@ struct QuestionModel {
             Question(text: "Only female koalas can whistle",
                 answers:
                 [
-                    Answer(text: "True", isCorrect: false),
-                    Answer(text: "False", isCorrect: true),
+                    Answer(text: "Yes", isCorrect: false),
+                    Answer(text: "No", isCorrect: true),
                 ]),
             Question(text: "Blue whales are technically whales",
                 answers:
                 [
-                    Answer(text: "True", isCorrect: true),
-                    Answer(text: "False", isCorrect: false),
+                    Answer(text: "Yes", isCorrect: true),
+                    Answer(text: "No", isCorrect: false),
                 ]),
             Question(text: "Camels are cannibalistic",
                 answers:
                 [
-                    Answer(text: "True", isCorrect: false),
-                    Answer(text: "False", isCorrect: true),
+                    Answer(text: "Yes", isCorrect: false),
+                    Answer(text: "No", isCorrect: true),
                 ]),
             Question(text: "All ducks are birds",
                 answers:
                 [
-                    Answer(text: "True", isCorrect: true),
-                    Answer(text: "False", isCorrect: false),
+                    Answer(text: "Yes", isCorrect: true),
+                    Answer(text: "No", isCorrect: false),
                 ]),
+            Question(text: "This was the only US President to serve more than two consecutive terms.",
+                answers:
+                [
+                    Answer(text: "George Washington", isCorrect: false),
+                    Answer(text: "Franklin D. Roosevelt", isCorrect: true),
+                    Answer(text: "Woodrow Wilson", isCorrect: false),
+                    Answer(text: "Andrew Jackson", isCorrect: false),
+                ]),
+            Question(text: "Which of the following countries has the most residents?",
+                answers:
+                [
+                    Answer(text: "Nigeria", isCorrect: true),
+                    Answer(text: "Russia", isCorrect: false),
+                    Answer(text: "Iran", isCorrect: false),
+                    Answer(text: "Vietnam", isCorrect: false),
+                ]),
+            Question(text: "The Titanic departed from the United Kingdom, where was it supposed to arrive?",
+                answers:
+                [
+                    Answer(text: "Paris", isCorrect: false),
+                    Answer(text: "Washington D.C.", isCorrect: false),
+                    Answer(text: "New York City", isCorrect: true),
+                    Answer(text: "Boston", isCorrect: false),
+                ]),
+            Question(text: "Which nation produces the most oil?",
+                answers:
+                [
+                    Answer(text: "Iran", isCorrect: false),
+                    Answer(text: "Iraq", isCorrect: false),
+                    Answer(text: "Brazil", isCorrect: false),
+                    Answer(text: "Canada", isCorrect: true),
+                ]),
+            Question(text: "Which country has most recently won consecutive World Cups in Soccer?",
+                answers:
+                [
+                    Answer(text: "Italy", isCorrect: false),
+                    Answer(text: "Brazil", isCorrect: true),
+                    Answer(text: "Argetina", isCorrect: false),
+                    Answer(text: "Spain", isCorrect: false),
+                ]),
+            Question(text: "Which of the following rivers is longest?",
+                answers:
+                [
+                    Answer(text: "Yangtze", isCorrect: false),
+                    Answer(text: "Mississippi", isCorrect: true),
+                    Answer(text: "Congo", isCorrect: false),
+                    Answer(text: "Mekong", isCorrect: false),
+                ]),
+            Question(text: "Which city is the oldest?",
+                answers:
+                [
+                    Answer(text: "Mexico City", isCorrect: true),
+                    Answer(text: "Cape Town", isCorrect: false),
+                    Answer(text: "San Juan", isCorrect: false),
+                    Answer(text: "Sydney", isCorrect: false),
+                ]),
+            Question(text: "Which country was the first to allow women to vote in national elections?",
+                answers:
+                [
+                    Answer(text: "Poland", isCorrect: true),
+                    Answer(text: "United States", isCorrect: false),
+                    Answer(text: "Sweden", isCorrect: false),
+                    Answer(text: "Senegal", isCorrect: false),
+                ]),
+            Question(text: "Which of these countries won the most medals in the 2012 Summer Games?",
+                answers:
+                [
+                    Answer(text: "France", isCorrect: false),
+                    Answer(text: "Germany", isCorrect: false),
+                    Answer(text: "Japan", isCorrect: false),
+                    Answer(text: "Great Britian", isCorrect: true),
+                ])
         ]
     
     func getRandomQuestion() -> Question {
@@ -104,11 +176,22 @@ struct QuestionModel {
     func getRandomAnswers(numberOfAnswers: Int, correctAnswer: Int) -> [Answer] {
         
         var randomAnswers: [Answer] = []
-        let maxNumberForIncorrectAnswer = 1000
-        let maxNumberClosestToCorrectAnswer = 100
+        
         let positionForCorrectAnswer = randomNumberModel.getWithLowerAndUpperBound(lowerBound: 1, upperBound: numberOfAnswers)
         let positionForClosestCorrectAnswer = randomNumberModel.getWithUpperBoundAndRestrictions(upperBound: numberOfAnswers, restrictions: [positionForCorrectAnswer])
         
+        let maxIntervalClosestToCorrectAnswer = 10
+        let highestLimitClosestToCorrectAnswer = correctAnswer + maxIntervalClosestToCorrectAnswer
+        let lowestLimitClosestToCorrectAnwser = correctAnswer - maxIntervalClosestToCorrectAnswer < 0
+                                              ? 0
+                                              : correctAnswer - maxIntervalClosestToCorrectAnswer
+
+        let maxIntervalAwayFromCorrectAnswer = 100
+        let highestLimitAwayFromCorrectAnswer = correctAnswer + maxIntervalAwayFromCorrectAnswer
+        let lowestLimitAwayFromCorrectAnwser = correctAnswer - maxIntervalAwayFromCorrectAnswer < 0
+                                             ? 0
+                                             : correctAnswer - maxIntervalAwayFromCorrectAnswer
+      
         for answer in 1...numberOfAnswers {
             
             if answer == positionForCorrectAnswer {
@@ -117,11 +200,11 @@ struct QuestionModel {
             }
             
             if answer == positionForClosestCorrectAnswer {
-                randomAnswers.append(Answer(text: String(randomNumberModel.getWithUpperBound(maxNumberClosestToCorrectAnswer) + correctAnswer), isCorrect: false))
+                randomAnswers.append(Answer(text: String(randomNumberModel.getWithLowerUpperBoundAndRestrictions(lowerBound: lowestLimitClosestToCorrectAnwser, upperBound: highestLimitClosestToCorrectAnswer, restrictions: [correctAnswer])), isCorrect: false))
                 continue
             }
             
-            randomAnswers.append(Answer(text: String(randomNumberModel.getWithUpperBound(maxNumberForIncorrectAnswer)), isCorrect: false))
+            randomAnswers.append(Answer(text: String(randomNumberModel.getWithLowerUpperBoundAndRestrictions(lowerBound: lowestLimitAwayFromCorrectAnwser, upperBound: highestLimitAwayFromCorrectAnswer, restrictions: [correctAnswer])), isCorrect: false))
         }
         
         return randomAnswers
