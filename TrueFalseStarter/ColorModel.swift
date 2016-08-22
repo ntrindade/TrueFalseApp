8660b19df9d3a73724ea17902cfa49f18e0aa29d
Nuno Trindade <nunomtrindade@gmail.com>
2016-08-22 23:23:08 +0100

    [True False App] Adds a Color Model struct to the project

---
+++ TrueFalseStarter/ColorModel.swift
@@ -0,0 +1,25 @@
+//
+//  ColorModel.swift
+//  TrueFalseStarter
+//
+//  Created by Nuno Trindade on 22/08/16.
+//  Copyright © 2016 Treehouse. All rights reserved.
+//
+
+import UIKit
+
+struct ColorModel {
+    let correctAnswerHeaderColor = UIColor(red:0/255.0, green:148/255.0, blue:135/255.0, alpha:1.0)
+    let incorrectAnswerHeaderColor = UIColor(red:255/255.0, green:128/255.0, blue:79/255, alpha:1.0)
+    
+    let nextActionTintColor = UIColor(red:87/255.0, green:186/255.0, blue:156/255.0, alpha:1.0)
+    
+    let enabledBackgroundColor = UIColor(red:12/255.0, green:121/255.0, blue:150/255.0, alpha:1.0)
+    let enabledTitleColor = UIColor(red:255/255.0, green:255/255.0, blue:255/255.0, alpha:1.0)
+    
+    let disabledBackgroundColor = UIColor(red:21/255, green:66/255.0, blue:99/255.0, alpha:1.0)
+    let disabledTitleColor = UIColor(red:68/255.0, green:75/255.0, blue:89/255.0, alpha:1.0)
+    
+    let incorrectTitleColor = UIColor(red:179/255.0, green:79/255.0, blue:110/255.0, alpha:1.0)
+    let correctTitleColor = UIColor(red:0/255.0, green:148/255.0, blue:135/255.0, alpha:1.0)
+}
