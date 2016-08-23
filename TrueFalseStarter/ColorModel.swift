b506a14c4ad5317c1a0358d890c9464b096d0edc
Nuno Trindade <nunomtrindade@gmail.com>
2016-08-23 01:15:22 +0100

    [True False App] Minor changes in color codes

--- TrueFalseStarter/ColorModel.swift
+++ TrueFalseStarter/ColorModel.swift
@@ -18,8 +18,8 @@ struct ColorModel {
     let enabledTitleColor = UIColor(red:255/255.0, green:255/255.0, blue:255/255.0, alpha:1.0)
     
     let disabledBackgroundColor = UIColor(red:21/255, green:66/255.0, blue:99/255.0, alpha:1.0)
-    let disabledTitleColor = UIColor(red:68/255.0, green:75/255.0, blue:89/255.0, alpha:1.0)
+    let disabledTitleColor = UIColor(red:100/255.0, green:100/255.0, blue:100/255.0, alpha:1.0)
     
-    let incorrectTitleColor = UIColor(red:179/255.0, green:79/255.0, blue:110/255.0, alpha:1.0)
-    let correctTitleColor = UIColor(red:0/255.0, green:148/255.0, blue:135/255.0, alpha:1.0)
+    let incorrectTitleColor = UIColor(red:245/255.0, green:191/255.0, blue:191/255.0, alpha:1.0)
+    let correctTitleColor = UIColor(red:168/255.0, green:247/255.0, blue:233/255.0, alpha:1.0)
 }
